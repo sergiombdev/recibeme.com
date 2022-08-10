7f82bcfdda4559115921eab5607fc309cec66060
sergiombdev <pxndxkirx@gmail.com>
2022-08-10 14:21:10 -0400

    local chnges

--- src/config/host.config.js
+++ src/config/host.config.js
@@ -1,7 +1,7 @@
 export const data = {
-	baseURL: `${window.location.origin}/api`,
-	host: window.location.origin,
-	// baseURL: `http://localhost:8080/api`,
-	// host: `http://localhost:8080`,
+	// baseURL: `${window.location.origin}/api`,
+	// host: window.location.origin,
+	baseURL: `https://api.recibeme.com/api`,
+	host: `https://api.recibeme.com`,
 	timeout: 1200000,
 };
